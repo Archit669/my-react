a1d2d7b1c75c0563213c2775471b8197414eeb5d
archit669 <architjain669@gmail.com>
2024-09-29 22:14:29 +0530

    useState hook , rerender still need to figure out

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import React from "../lib/react.js";
+import { useState } from "../lib/react.js";
 
 export default function App(props){
+    const [val, setValue] = useState(9);
+    const [val2, setValue2] = useState(11);
+
     return (
         React.createElement("div", {}, [
-            React.createElement("h1", null, "Hello World"),
-            React.createElement("p", null, "I am a React component"),
+            React.createElement("h1", null, `${val}`),
+            React.createElement("h1", null, `${val2}`),
             React.createElement("p", null, "I am a React component"),
             React.createElement("p", null, [
                 React.createElement("p", null, `${props.name}`)
             ]),
         ])
     )
-}currentVNode
+}
